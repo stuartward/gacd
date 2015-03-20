f118e92f8a7f643f529b578d4dc48159f87e5a89
stuartward <stuart@stuartward.com>
2015-03-20 00:27:20 -0400

    Third commit
    Third commit

--- run_analysis.R
+++ run_analysis.R
@@ -17,3 +17,35 @@ subject_test <- read.table("test/subject_test.txt")
 ##Import feature names and activity labels
 features <- read.table("features.txt")[,2]
 activity_labels <- read.table("activity_labels.txt")[,2]
+
+
+##Extract mean and standard deviation; set activity labels
+extract_features <- grepl("mean|std", features)
+names(X_test) = features
+X_test <- X_test[,extract_features]
+y_test[,2] <- activity_labels[y_test[,1]]
+names(y_test) <- c("ActivityID", "ActivityLabel")
+names(subject_test) <- "Subject"
+
+TestData <- cbind(as.data.table(subject_test), y_test, X_test)
+
+##Training data
+X_train <- read.table("train/X_train.txt")
+y_train <- read.table("train/y_train.txt")
+subject_train <- read.table("train/subject_train.txt")
+names(X_train) = features
+
+##Extract mean and standard deviation; set activity labels
+X_train <- X_train[,extract_features]
+y_train[,2] <- activity_labels[y_train[,1]]
+names(y_train) <- c("ActivityID", "ActivityLabel")
+names(subject_train) <- "Subject"
+
+TrainingData <- cbind(as.data.table(subject_train), y_train, X_train)
+
+##Create data set
+MergedData <- rbind(TestData, TrainingData, fill=TRUE)
+IDlabels <- c("Subject", "ActivityID", "ActivityLabel")
+DataLabels <- setdiff(colnames(MergedData), IDlabels)
+TempResult <- melt(MergedData, id = IDlabels, measure.vars = DataLabels)
+FinalResult <- dcast(TempResult, Subject + ActivityLabel ~ variable, mean)
